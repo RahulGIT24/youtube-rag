8b9b588cf67d85d5cb8b995c687792334b8b63a2
rahul <rg4005450@gmail.com>
2025-12-28 22:28:10 +0530

    logger added

--- server/workers/transcribe_worker.py
+++ server/workers/transcribe_worker.py
@@ -5,21 +5,46 @@ from utils.qdrant import get_client
 from utils.embedding_models import dense_embedding,sparse_embedding
 from dotenv import load_dotenv
 import os
+import logging
 import uuid
 from qdrant_client import models
 
+logging.basicConfig(filename="transcribe_worker.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a',
+                    level=logging.INFO)
 load_dotenv()
 
 obj = Transcript()
 qdrant_client=get_client()
 
 QUEUE_NAME="queue:pending"
+COLLECTION_NAME=os.getenv('QDRANT_COLLECTION')
 
 def process_job(job:dict):
     try:
         video_id=job['video_id']
 
-        print("Processing Video ID "+video_id)
+        if not video_id:
+            raise ValueError("Video Id not found")
+
+        exists = qdrant_client.scroll(COLLECTION_NAME,
+            scroll_filter=models.Filter(
+                must=[
+                    models.FieldCondition(
+                        key="video_id",
+                        match=models.MatchValue(value=video_id),
+                    ),
+                ],
+            )
+        )
+
+        if(len(exists)>0):
+            logging.info("Already Stored. Skipping Processing for video id " + video_id)
+            return
+
+
+        logging.info("Processing Video ID "+video_id)
 
         chunks = obj.fetch_transcript(video_id=video_id)
 
@@ -39,7 +64,6 @@ def process_job(job:dict):
                     "video_id":video_id
                 },
                 vector={
-                    # Key must match the config names above
                     "dense-text": dense,
                     "sparse-text": models.SparseVector(
                         indices=sparse.indices.tolist(),
@@ -48,23 +72,23 @@ def process_job(job:dict):
                 }
             ))
 
-        qdrant_client.upsert(collection_name=os.getenv('QDRANT_COLLECTION'),points=points)
-        print("Processed Video Id -> "+video_id)
+        qdrant_client.upsert(collection_name=COLLECTION_NAME,points=points)
+        logging.info("Processed Video Id -> "+video_id)
     except Exception as e:
         raise e
 
 def start_worker():
-    print("Worker Started")
+    logging.info("Worker Started")
 
     while True:
-        print("Waiting For Jobs......")
+        logging.info("Waiting For Jobs......")
         try:
             _,data=redis_client.blpop(QUEUE_NAME)
             job = json.loads(data)
             process_job(job)
 
         except Exception as e:
-            print('Error while processing jobs. ',e)
+            logging.error('Error while processing jobs. ',e)
 
 if __name__ == "__main__":
     start_worker()
